7b7451fa4362fd453f4a21e06a4525bdb8385757
Thierry-HUET <thierry.huet@aperto-nota.fr>
2026-03-03 17:59:41 +0100

    Create init.sql

---
+++ init.sql
@@ -0,0 +1,12 @@
+-- ============================================================================
+--  Copyright (c) 2026 Thierry HUET
+
+-- Licensed under the Apache License, Version 2.0 (the "License"),
+-- with the Commons Clause restriction.
+-- You may not sell this software or provide it as part of a paid service.
+
+-- ============================================================================
+CREATE SCHEMA IF NOT EXISTS urban;
+
+-- UUID generation
+CREATE EXTENSION IF NOT EXISTS pgcrypto;
