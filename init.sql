d2bcb7bb0f5819e4776a9493b1280fab40db2607
Thierry-HUET <thierry.huet@aperto-nota.fr>
2026-03-03 19:08:08 +0100

    Update init.sql

--- init.sql
+++ init.sql
@@ -6,7 +6,33 @@
 -- You may not sell this software or provide it as part of a paid service.
 
 -- ============================================================================
-CREATE SCHEMA IF NOT EXISTS urban;
+CREATE SCHEMA IF NOT EXISTS urba;
 
 -- UUID generation
 CREATE EXTENSION IF NOT EXISTS pgcrypto;
+
+-- 
+-- urba.metadata definition
+
+CREATE TABLE urba.metadata (
+	id serial4 NOT NULL,
+	created_at timestamp NULL,
+	updated_at timestamp NULL,
+	created_by varchar NULL,
+	updated_by varchar NULL,
+	nc_order numeric NULL,
+	meta_key text NULL,
+	meta_value text NULL,
+  localisation text NULL,
+	CONSTRAINT metadata_pkey PRIMARY KEY (id)
+);
+CREATE INDEX metadata_order_idx ON urba.metadata USING btree (nc_order);
+
+-- Insertions
+INSERT INTO urba.metadata (nc_order,meta_key,meta_value,localisation) VALUES
+	 (2,'statut','Actif','fr'),
+	 (3,'statut','En cours','fr'),
+	 (4,'statut','Clôturé','fr'),
+	 (1,'lang','fr','fr'),
+	 (5,'statut','Terminé','fr');
+
